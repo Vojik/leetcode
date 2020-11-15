3a141ed66c56cdcc62c0fb1c8c836ebe0dcc351d
Vojik <siarhei.komlik@gmail.com>
2020-11-14 21:42:25 -0800

    Add solution 283 Move Zeroes

---
+++ src/main/java/com/vojik/solutions/Solution_283.java
@@ -0,0 +1,39 @@
+package com.vojik.solutions;
+
+/**
+ * 283. Move Zeroes
+ *
+ * <p>Given an array nums, write a function to move all 0's to the end of it while maintaining the
+ * relative order of the non-zero elements.
+ *
+ * <p>Example:
+ *
+ * <p>Input: [0,1,0,3,12] Output: [1,3,12,0,0]
+ * <p>
+ * Note:
+ * <p>You must do this in-place without making a copy of the array. Minimize the total number of
+ * operations.
+ */
+public class Solution_283 {
+
+  // Time: O(n) Space: O(1)
+  public void moveZeroes(int[] nums) {
+    if (nums == null) {
+      return;
+    }
+    int zeroPointer = -1;
+    int i = 0;
+
+    while (i < nums.length) {
+      if (nums[i] != 0 && zeroPointer >= 0) {
+        nums[zeroPointer] = nums[i];
+        nums[i] = 0;
+        zeroPointer++;
+      } else if (zeroPointer < 0) {
+        zeroPointer = i;
+      }
+      i++;
+    }
+  }
+
+}
---
+++ src/test/java/com/vojik/solutions/Solution_283Test.java
@@ -0,0 +1,36 @@
+package com.vojik.solutions;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+class Solution_283Test {
+
+  @Test
+  public void test1_whenSolution() {
+    Solution_283 solution = new Solution_283();
+    int[] input = new int[]{0, 1, 0, 3, 12};
+    int[] expected = new int[]{1, 3, 12, 0, 0};
+    solution.moveZeroes(input);
+    assertArrayEquals(expected, input);
+  }
+
+  @Test
+  public void test2_whenSolution() {
+    Solution_283 solution = new Solution_283();
+    int[] input = null;
+    int[] expected = null;
+    solution.moveZeroes(input);
+    assertArrayEquals(expected, input);
+  }
+
+  @Test
+  public void test3_whenSolution() {
+    Solution_283 solution = new Solution_283();
+    int[] input = new int[]{};
+    int[] expected = new int[]{};
+    solution.moveZeroes(input);
+    assertArrayEquals(expected, input);
+  }
+
+}
