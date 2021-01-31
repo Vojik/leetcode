ce1728d28c6de544d88c4779ce81619fce6839e9
Vojik <siarhei.komlik@gmail.com>
2021-01-30 23:16:30 -0800

    Add solution 1641. Count Sorted Vowel Strings

--- src/main/java/com/vojik/solutions/Solution_1641.java
+++ src/main/java/com/vojik/solutions/Solution_1641.java
@@ -4,18 +4,34 @@ public class Solution_1641 {
 
   private static final char[] VOWELS = new char[]{'a','e','i','o','u'};
 
-  public int countVowelStrings(int n) {
-    return countVowelStrings(n, "", 0);
+  public int countVowelStringsBacktracking(int n) {
+    return countVowelStringsBacktracking(n, "", 0);
   }
 
-  private int countVowelStrings(int n, String str, int start) {
+  private int countVowelStringsBacktracking(int n, String str, int start) {
     if (n == str.length()) {
       return 1;
     }
 
     int res = 0;
     for (int i = start; i < VOWELS.length; i++) {
-      res += countVowelStrings(n, str + VOWELS[i], i);
+      res += countVowelStringsBacktracking(n, str + VOWELS[i], i);
+    }
+    return res;
+  }
+
+  public int countVowelStringsBacktrackingOptimized(int n) {
+    return countVowelStringsBacktrackingOptimized(n, 0);
+  }
+
+  private int countVowelStringsBacktrackingOptimized(int n, int start) {
+    if (n == 0) {
+      return 1;
+    }
+
+    int res = 0;
+    for (int i = start; i < 5; i++) {
+      res += countVowelStringsBacktrackingOptimized(n -1, i);
     }
     return res;
   }
--- src/test/java/com/vojik/solutions/Solution_1641Test.java
+++ src/test/java/com/vojik/solutions/Solution_1641Test.java
@@ -8,22 +8,45 @@ class Solution_1641Test {
 
   @Test
   public void test1_whenSolution1() {
-    assertEquals(15, new Solution_1641().countVowelStrings(2));
+    assertEquals(15, new Solution_1641().countVowelStringsBacktracking(2));
   }
 
   @Test
   public void test2_whenSolution1() {
-    assertEquals(5, new Solution_1641().countVowelStrings(1));
+    assertEquals(5, new Solution_1641().countVowelStringsBacktracking(1));
   }
 
   @Test
   public void test3_whenSolution1() {
-    assertEquals(316251, new Solution_1641().countVowelStrings(50));
+    assertEquals(316251, new Solution_1641().countVowelStringsBacktracking(50));
   }
 
   @Test
   public void test4_whenSolution1() {
-    assertEquals(35, new Solution_1641().countVowelStrings(3));
+    assertEquals(35, new Solution_1641().countVowelStringsBacktracking(3));
+  }
+
+
+  /////
+
+  @Test
+  public void test1_whenSolution2() {
+    assertEquals(15, new Solution_1641().countVowelStringsBacktracking(2));
+  }
+
+  @Test
+  public void test2_whenSolution2() {
+    assertEquals(5, new Solution_1641().countVowelStringsBacktracking(1));
+  }
+
+  @Test
+  public void test3_whenSolution2() {
+    assertEquals(316251, new Solution_1641().countVowelStringsBacktracking(50));
+  }
+
+  @Test
+  public void test4_whenSolution2() {
+    assertEquals(35, new Solution_1641().countVowelStringsBacktracking(3));
   }
 
 }
