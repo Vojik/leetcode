24695f60410690c11019bdf094212b14bd5cbc51
Vojik <siarhei.komlik@gmail.com>
2021-04-10 20:32:05 -0700

    Add solution 1448. Count Good Nodes in Binary Tree

--- src/main/java/com/vojik/solutions/Solution_1448.java
+++ src/main/java/com/vojik/solutions/Solution_1448.java
@@ -1,5 +1,26 @@
 package com.vojik.solutions;
 
+import com.vojik.helpers.TreeNode;
+
 public class Solution_1448 {
+
+  public int goodNodes(TreeNode root) {
+    return dfs(root, Integer.MIN_VALUE);
+  }
+
+  private int dfs(TreeNode node, int max) {
+    if (node == null) {
+      return 0;
+    }
+    int count = 0;
+    if (node.val >= max) {
+      count++;
+      max = node.val;
+    }
+
+    count += dfs(node.left, max);
+    count += dfs(node.right, max);
+    return count;
+  }
 
 }
