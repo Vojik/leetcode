d977637667cc794047587714f7b6260106b39363
Vojik <siarhei.komlik@gmail.com>
2024-08-22 16:16:57 -0700

    Add another solution for 153. Find Minimum in Rotated Sorted Array

--- src/main/java/com/vojik/solutions/Solution_153.java
+++ src/main/java/com/vojik/solutions/Solution_153.java
@@ -37,4 +37,20 @@ public class Solution_153 {
     }
     return nums[left];
   }
+
+  public int findMin2(int[] nums) {
+    int lo = 0;
+    int hi = nums.length - 1;
+    int ans = Integer.MAX_VALUE;
+    while (lo <= hi) {
+      int mid = lo + (hi - lo) / 2;
+      if (nums[mid] > nums[hi]) {
+        lo = mid + 1;
+      } else {
+        ans = Math.min(nums[mid],ans);
+        hi = mid - 1;
+      }
+    }
+    return ans;
+  }
 }
--- src/test/java/com/vojik/solutions/Solution_153Test.java
+++ src/test/java/com/vojik/solutions/Solution_153Test.java
@@ -34,4 +34,34 @@ class Solution_153Test {
     assertEquals(2, actual);
   }
 
+  ////////////////////////////
+
+  @Test
+  public void test1_whenSolution2() {
+    Solution_153 solution = new Solution_153();
+    int actual = solution.findMin2(new int[]{3, 4, 5, 1, 2});
+    assertEquals(1, actual);
+  }
+
+  @Test
+  public void test2_whenSolution2() {
+    Solution_153 solution = new Solution_153();
+    int actual = solution.findMin2(new int[]{4,5,6,7,0,1,2});
+    assertEquals(0, actual);
+  }
+
+  @Test
+  public void test3_whenSolution2() {
+    Solution_153 solution = new Solution_153();
+    int actual = solution.findMin2(new int[]{3,1,2});
+    assertEquals(1, actual);
+  }
+
+  @Test
+  public void test4_whenSolution2() {
+    Solution_153 solution = new Solution_153();
+    int actual = solution.findMin2(new int[]{2});
+    assertEquals(2, actual);
+  }
+
 }
