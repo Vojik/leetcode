eaa02f5b90ab9bbea62839429e3be4eb88701075
Vojik <siarhei.komlik@gmail.com>
2024-08-26 22:16:14 -0700

    Add solution 154. Find Minimum in Rotated Sorted Array II

--- src/main/java/com/vojik/solutions/Solution_154.java
+++ src/main/java/com/vojik/solutions/Solution_154.java
@@ -27,4 +27,26 @@ public class Solution_154 {
     }
     return nums[left];
   }
+
+  public int findMin2(int[] nums) {
+    int right = nums.length - 1;
+    int lo = 0;
+    int hi = right;
+    int ans = -1;
+
+    while (lo < hi && nums[lo] == nums[hi]) {
+      lo++;
+    }
+
+    while (lo <= hi) {
+      int mid = lo + (hi - lo) / 2;
+      if (nums[mid] <= nums[right]) {
+        ans = nums[mid];
+        hi = mid - 1;
+      } else {
+        lo = mid + 1;
+      }
+    }
+    return ans;
+  }
 }
--- src/test/java/com/vojik/solutions/Solution_154Test.java
+++ src/test/java/com/vojik/solutions/Solution_154Test.java
@@ -18,4 +18,16 @@ class Solution_154Test {
     assertEquals(1, actual);
   }
 
+  @Test
+  public void test1_whenSolution2() {
+    int actual = new Solution_154().findMin2(new int[]{2, 2, 2, 0, 1});
+    assertEquals(0, actual);
+  }
+
+  @Test
+  public void test2_whenSolution2() {
+    int actual = new Solution_154().findMin2(new int[]{3, 3, 1, 3});
+    assertEquals(1, actual);
+  }
+
 }
