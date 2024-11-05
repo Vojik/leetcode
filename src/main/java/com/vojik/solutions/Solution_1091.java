74296eec0e19ef44861d512fdbeaa3c2929fa1fc
Vojik <siarhei.komlik@gmail.com>
2024-11-04 19:14:32 -0800

    Add BFS solution for 1091. Shortest Path in Binary Matrix

--- src/main/java/com/vojik/solutions/Solution_1091.java
+++ src/main/java/com/vojik/solutions/Solution_1091.java
@@ -12,7 +12,7 @@ public class Solution_1091 {
       {1, 0}, {-1, 0}, {-1, -1}};
 
   // Time: O(n) Space: O(n)
-  public int shortestPathBinaryMatrix(int[][] grid) {
+  public int shortestPathBinaryMatrix_bfs(int[][] grid) {
     int len = grid.length;
     if (grid[0][0] == 1 || grid[len - 1][len - 1] == 1) {
       return -1;
@@ -46,6 +46,42 @@ public class Solution_1091 {
       }
     }
     return -1;
+  }
+
+  public int shortestPathBinaryMatrix_dfs_TLE(int[][] grid) {
+    int n = grid[0].length;
+    if (grid[0][0] != 0 || grid[n - 1][n - 1] != 0) {
+      return -1;
+    }
+
+    int[][] seen = new int[n][n];
+
+    dfs(grid, seen, n, 0, 0, 1);
+    return seen[n - 1][n - 1] != 0 ? seen[n - 1][n - 1] : -1;
+  }
+
+  private void dfs(int[][] grid, int[][] seen, int n, int x, int y, int pathValue) {
+    if (!isValidCell(x, y, n) || grid[x][y] != 0) {
+      return;
+    }
+
+    if (seen[x][y] == 0 || seen[x][y] >= pathValue) {
+      seen[x][y] = pathValue;
+    } else if (seen[x][y] < pathValue) {
+      return;
+    }
+
+    if (x == n - 1 && y == n - 1) {
+      return;
+    }
+
+    for (int[] dir : DIRECTIONS) {
+      dfs(grid, seen, n, x + dir[0], y + dir[1], pathValue + 1);
+    }
+  }
+
+  private boolean isValidCell(int x, int y, int n) {
+    return x >= 0 && x < n && y >= 0 && y < n;
   }
 
 }
--- src/test/java/com/vojik/solutions/Solution_1091Test.java
+++ src/test/java/com/vojik/solutions/Solution_1091Test.java
@@ -9,13 +9,38 @@ class Solution_1091Test {
   @Test
   public void test1_whenSolution1() {
     assertEquals(4,
-        new Solution_1091().shortestPathBinaryMatrix(new int[][]{{0, 0, 0}, {1, 1, 0}, {1, 1, 0}}));
+        new Solution_1091().shortestPathBinaryMatrix_bfs(new int[][]{{0, 0, 0}, {1, 1, 0}, {1, 1, 0}}));
   }
 
   @Test
   public void test2_whenSolution1() {
     assertEquals(2,
-        new Solution_1091().shortestPathBinaryMatrix(new int[][]{{0, 1}, {1, 0}}));
+        new Solution_1091().shortestPathBinaryMatrix_bfs(new int[][]{{0, 1}, {1, 0}}));
+  }
+
+  @Test
+  public void test3_whenSolution1() {
+    assertEquals(4,
+        new Solution_1091().shortestPathBinaryMatrix_bfs(new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}));
+  }
+
+
+  @Test
+  public void test1_whenSolution2() {
+    assertEquals(4,
+        new Solution_1091().shortestPathBinaryMatrix_dfs_TLE(new int[][]{{0, 0, 0}, {1, 1, 0}, {1, 1, 0}}));
+  }
+
+  @Test
+  public void test2_whenSolution2() {
+    assertEquals(2,
+        new Solution_1091().shortestPathBinaryMatrix_dfs_TLE(new int[][]{{0, 1}, {1, 0}}));
+  }
+
+  @Test
+  public void test3_whenSolution2() {
+    assertEquals(4,
+        new Solution_1091().shortestPathBinaryMatrix_dfs_TLE(new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}));
   }
 
 }
