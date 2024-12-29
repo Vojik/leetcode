83181b58ea554ba697563049b7fcb92c74c2c339
Vojik <siarhei.komlik@gmail.com>
2024-12-29 14:49:51 -0800

    Add solution 523. Continuous Subarray Sum

--- src/main/java/com/vojik/solutions/Solution_523.java
+++ src/main/java/com/vojik/solutions/Solution_523.java
@@ -3,22 +3,23 @@ package com.vojik.solutions;
 import java.util.HashMap;
 import java.util.Map;
 
-/** 523. Continuous Subarray Sum */
+/**
+ * 523. Continuous Subarray Sum
+ */
 public class Solution_523 {
 
   public boolean checkSubarraySum(int[] nums, int k) {
     Map<Integer, Integer> modIndex = new HashMap<>();
     modIndex.put(0, -1);
-    int sum = 0;
+    int prefixMod = 0;
     for (int i = 0; i < nums.length; i++) {
-      sum += nums[i];
-      if (k != 0) sum %= k;
-      if (modIndex.containsKey(sum)) {
-        if (i - modIndex.get(sum) > 1) {
+      prefixMod = (prefixMod + nums[i]) % k;
+      if (modIndex.containsKey(prefixMod)) {
+        if (i - modIndex.get(prefixMod) > 1) {
           return true;
         }
       } else {
-        modIndex.put(sum, i);
+        modIndex.put(prefixMod, i);
       }
     }
     return false;
--- src/test/java/com/vojik/solutions/Solution_523Test.java
+++ src/test/java/com/vojik/solutions/Solution_523Test.java
@@ -9,21 +9,41 @@ class Solution_523Test {
 
   @Test
   public void test1_whenSolution1() {
-    assertTrue(new Solution_523().checkSubarraySum(new int[] {23, 2, 4, 6, 7}, 6));
+    assertTrue(new Solution_523().checkSubarraySum(new int[]{23, 2, 4, 6, 7}, 6));
   }
 
   @Test
   public void test2_whenSolution1() {
-    assertTrue(new Solution_523().checkSubarraySum(new int[] {23, 2, 6, 4, 7}, 6));
+    assertTrue(new Solution_523().checkSubarraySum(new int[]{23, 2, 6, 4, 7}, 6));
   }
 
   @Test
   public void test3_whenSolution1() {
-    assertFalse(new Solution_523().checkSubarraySum(new int[] {23, 2, 6, 4, 7}, 13));
+    assertFalse(new Solution_523().checkSubarraySum(new int[]{23, 2, 6, 4, 7}, 13));
   }
 
   @Test
   public void test4_whenSolution1() {
-    assertTrue(new Solution_523().checkSubarraySum(new int[] {5, 0, 0, 0}, 3));
+    assertTrue(new Solution_523().checkSubarraySum(new int[]{5, 0, 0, 0}, 3));
+  }
+
+  @Test
+  public void test5_whenSolution1() {
+    assertFalse(new Solution_523().checkSubarraySum(new int[]{0}, 1));
+  }
+
+  @Test
+  public void test6_whenSolution1() {
+    assertFalse(new Solution_523().checkSubarraySum(new int[]{0, 1, 0, 3, 0, 4, 0, 4, 0}, 5));
+  }
+
+  @Test
+  public void test7_whenSolution1() {
+    assertTrue(new Solution_523().checkSubarraySum(new int[]{1, 2, 3}, 5));
+  }
+
+  @Test
+  public void test8_whenSolution1() {
+    assertFalse(new Solution_523().checkSubarraySum(new int[]{5, 2, 4}, 5));
   }
 }
