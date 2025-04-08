3be93ed29019a032fd17c8a5d2b9f4da38684592
Vojik <siarhei.komlik@gmail.com>
2025-04-08 11:33:47 -0700

    Add solution 380. Insert Delete GetRandom O(1)

--- src/main/java/com/vojik/solutions/Solution_380.java
+++ src/main/java/com/vojik/solutions/Solution_380.java
@@ -1,46 +1,46 @@
 package com.vojik.solutions;
 
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
-import java.util.Set;
 
 /** 380. Insert Delete GetRandom O(1) */
 public class Solution_380 {
-  private final List<Integer> list;
-  private final Set<Integer> set;
-  private final Random random;
 
-  /** Initialize your data structure here. */
+  private List<Integer> list;
+  private Map<Integer, Integer> map;
+  private Random random = new Random();
+
   public Solution_380() {
     this.list = new ArrayList<>();
-    this.set = new HashSet<>();
-    this.random = new Random();
+    this.map = new HashMap<>();
   }
 
-  /**
-   * Inserts a value to the set. Returns true if the set did not already contain the specified
-   * element.
-   */
   public boolean insert(int val) {
-    boolean wasAdded = set.add(val);
-    if (wasAdded) {
-      list.add(val);
+    if (map.containsKey(val)) {
+      return false;
     }
-    return wasAdded;
+    map.put(val, list.size());
+    list.add(val);
+    return true;
   }
 
-  /** Removes a value from the set. Returns true if the set contained the specified element. */
   public boolean remove(int val) {
-    boolean wasRemoved = set.remove(val);
-    if (wasRemoved) {
-      list.remove(Integer.valueOf(val));
+    if (!map.containsKey(val)) {
+      return false;
     }
-    return wasRemoved;
+
+    int lastElement = list.get(list.size() - 1);
+    int idx = map.get(val);
+    list.set(idx, lastElement);
+    list.remove(list.size() - 1);
+    map.put(lastElement, idx);
+    map.remove(val);
+    return true;
   }
 
-  /** Get a random element from the set. */
   public int getRandom() {
     return list.get(random.nextInt(list.size()));
   }
