d474238f3e5ffd4d95c99ace28164f3b86b5815e
Vojik <siarhei.komlik@gmail.com>
2025-05-13 20:14:29 -0700

    Add solution 148. Sort List

---
+++ src/main/java/com/vojik/solutions/Solution_148.java
@@ -0,0 +1,28 @@
+package com.vojik.solutions;
+
+import com.vojik.helpers.ListNode;
+import java.util.PriorityQueue;
+
+public class Solution_148 {
+
+  public ListNode sortList(ListNode head) {
+    PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+
+    ListNode cur = head;
+    while (cur != null) {
+      ListNode next = cur.next;
+      cur.next = null;
+      pq.add(cur);
+      cur = next;
+    }
+
+    ListNode dummyNode = new ListNode();
+    ListNode current = dummyNode;
+    while (!pq.isEmpty()) {
+      ListNode nextNode = pq.poll();
+      current.next = nextNode;
+      current = nextNode;
+    }
+    return dummyNode.next;
+  }
+}
